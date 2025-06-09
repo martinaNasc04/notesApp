15021822e4129e2d7e866c941927fca65eecb279
Martina <martinanascimento95@gmail.com>
2025-06-09 08:15:58 -0300

    Adicionando models e urls

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from .models import Note
 
 
 # Criando um serializador para o modelo User para retornar em JSON
@@ -12,3 +13,10 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
         return user
+
+
+class NoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Note
+        fields = ["id", "title", "content", "created_at", "author"]
+        extra_kwargs = {"author": {"read_only": True}}
